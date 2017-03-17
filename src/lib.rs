1dbb4e74bdb8cfd40ffb7f190d9be153c07556f2
bRad <brad@humanenginuity.com>
2017-03-17 16:09:57 -0700

    WIP: Implemented ArgsExtMethod trait to not consume Vec<_>'s.  Does not yet work for std::env::arg...

--- src/lib.rs
+++ src/lib.rs
@@ -6,64 +6,66 @@ unsafe_code, unused_import_braces, unused_qualifications)]
 #![feature(inclusive_range_syntax)]
 #![feature(associated_consts)]
 
-trait ArgsExtensionMethods<'a> where Self: IntoIterator<Item = &'a str> {
-    const NO_START_FILES_SWITCH: &'static str = "-nostartfiles";
-    const NO_DEFAULT_LIBS_SWITCH: &'static str = "-nodefaultlibs";
-    const WL_SWITCH_PREFIX: &'static str = "-Wl,";
-    const OUTPUT_SWITCH: &'static str = "-o";
+use std::path::Path;
 
-    fn get_output_filename(self) -> Option<&'a str>;
-    fn remove_first_arg(self) -> Vec<&'a str>;
-    fn remove_output_filename_switches_and_params(self) -> Vec<&'a str>;
-    fn remove_nostartfiles_nodefaultlibs_switches(self) -> Vec<&'a str>;
-    fn remove_wl_switches(self) -> Vec<&'a str>;
+trait ArgsExtensionMethods<'a> where Self: IntoIterator {
+//    const NO_START_FILES_SWITCH: &'static str = "-nostartfiles";
+//    const NO_DEFAULT_LIBS_SWITCH: &'static str = "-nodefaultlibs";
+//    const WL_SWITCH_PREFIX: &'static str = "-Wl,";
+
+    fn find_arg_after_switch(self, switch: &str) -> Option<Self::Item>;
+
+    //    fn remove_first_arg(self) -> Self;
+//    fn remove_output_filename_switches_and_params(self) -> Self;
+//    fn remove_nostartfiles_nodefaultlibs_switches(self) -> Self;
+//    fn remove_wl_switches(self) -> Self;
 }
 
-//TODO: If multiple -o params specified, return (last occurrence of) OUTPUT_FILENAME parameter, if present
-impl<'a, T> ArgsExtensionMethods<'a> for T where T: IntoIterator<Item = &'a str> {
-    fn get_output_filename(self) -> Option<&'a str> {
+impl<'a, T> ArgsExtensionMethods<'a> for T where T: IntoIterator<Item = &'a &'a str> {
+    //TODO: If multiple -o params specified, return (last occurrence of) OUTPUT_FILENAME parameter, if present
+    fn find_arg_after_switch(self, switch_name: &str) -> Option<Self::Item> {
         self.into_iter()
-            .skip_while(|&el| el != Self::OUTPUT_SWITCH)
-            .skip(1)    //move past -o switch
+            .skip_while(|el| **el != switch_name)
+            .skip(1) //move past -o switch
             .next()
     }
-
-    fn remove_first_arg(self) -> Vec<&'a str> {
-        self.into_iter()
-            .skip(1)
-            .take_while(|_| true)
-            .collect::<Vec<_>>()
-    }
-
-    //Delete all of both the OUTPUT_SWITCH and the immediately following argument (OUTPUT_FILENAME) if one is present
-    fn remove_output_filename_switches_and_params(self) -> Vec<&'a str> {
-        self.into_iter()
-            .scan(false, |found_switch, el| match *found_switch {
-                true => { *found_switch = false; Some(None) },
-                false => match el == Self::OUTPUT_SWITCH {
-                    true => { *found_switch = true; Some(None) },
-                    false => Some(Some(el)) }})
-            .filter(|el| el.is_some())
-            .map(|el| el.expect("Unwrapped element found in list of Option<&str>!"))
-            .collect::<Vec<_>>()
-    }
-
-    fn remove_nostartfiles_nodefaultlibs_switches(self) -> Vec<&'a str> {
-        self.into_iter()
-            .filter(|&el| el != Self::NO_START_FILES_SWITCH && el != Self::NO_DEFAULT_LIBS_SWITCH)
-            .collect::<Vec<_>>()
-    }
-
-    fn remove_wl_switches(self) -> Vec<&'a str> {
-        self.into_iter()
-            .map(|el| match el.starts_with(Self::WL_SWITCH_PREFIX) {
-                true => &el[Self::WL_SWITCH_PREFIX.as_bytes().len()..],
-                false => &el, })
-            .filter(|el| !el.trim().is_empty())
-            .collect::<Vec<_>>()
-    }
 }
-
+//    fn remove_first_arg(self) -> Vec<&'a str> {
+//        self.into_iter()
+//            .skip(1)
+//            .take_while(|_| true)
+//            .collect::<Vec<_>>()
+//    }
+//
+//    //Delete all of both the OUTPUT_SWITCH and the immediately following argument (OUTPUT_FILENAME) if one is present
+//    fn remove_output_filename_switches_and_params(self) -> Self {
+//        self.into_iter()
+//            .scan(false, |found_switch, el| match *found_switch {
+//                true => { *found_switch = false; Some(None) },
+//                false => match el == Self::OUTPUT_SWITCH {
+//                    true => { *found_switch = true; Some(None) },
+//                    false => Some(Some(el)) }})
+//            .filter(|el| el.is_some())
+//            .map(|el| el.expect("Unwrapped element found in list of Option<&str>!"))
+//            .collect::<Vec<_>>()
+//    }
+//
+//    fn remove_nostartfiles_nodefaultlibs_switches(self) -> Self {
+//        self.into_iter()
+//            .filter(|&el| el != Self::NO_START_FILES_SWITCH && el != Self::NO_DEFAULT_LIBS_SWITCH)
+//            .collect::<Vec<_>>()
+//    }
+//
+//    fn remove_wl_switches(self) -> Self {
+//        self.into_iter()
+//            .map(|el| match el.starts_with(Self::WL_SWITCH_PREFIX) {
+//                true => &el[Self::WL_SWITCH_PREFIX.as_bytes().len()..],
+//                false => &el, })
+//            .filter(|el| !el.trim().is_empty())
+//            .collect::<Vec<_>>()
+//    }
+//}
+//
 fn get_linker_uri() -> String
 {
     const LLD_PATH_KEY: &'static str = "LLD_PATH";
@@ -78,10 +80,18 @@ fn get_linker_uri() -> String
 //(implement std::convert::From<Vec<String>> for Vec<&str>)
 pub fn lib_main<'a, T>(args: T) where T: IntoIterator<Item=&'a str>
 {
-   let fixed_args = args.remove_first_arg()
-                         .remove_nostartfiles_nodefaultlibs_switches()
-                         .remove_wl_switches();
-    std::process::Command::new(get_linker_uri()).args(fixed_args).spawn().expect("lld command failed");
+    const OUTPUT_SWITCH_NAME: &'static str = "-o";
+    let fake_args = vec!["a", "b", "-o", "d"];
+
+    let target = Path::new(fake_args.find_arg_after_switch(OUTPUT_SWITCH_NAME).unwrap());
+//    let fixed_args = args.remove_first_arg()
+//                        .remove_nostartfiles_nodefaultlibs_switches()
+//                        .remove_wl_switches();
+//
+//    std::process::Command::new(get_linker_uri()).args(fixed_args).spawn().expect("lld command failed");
+//    std::process::Command::new("cp").args(&[target.to_str().unwrap(),
+//                                          &("./target/".to_string() + target.file_name().unwrap().to_str().unwrap())]);
+    println!("{:?}{:?}", target, Path::new(fake_args.find_arg_after_switch(OUTPUT_SWITCH_NAME).unwrap()));
 }
 
 #[cfg(test)]
--- src/tests.rs
+++ src/tests.rs
@@ -38,75 +38,73 @@ fn app_call_with_no_params_succeeds() {
     let f_handle = ScopedFile::new(Path::new(&(get_linker_uri())).to_path_buf());
     //Rust currently has no way to set the executable bit on a file, so the following will panic (permission denied)
     //when libMain() tries to run test-ld.lld.
-    let result = panic::catch_unwind(|| lib_main(Vec::<&str>::new()));
-    assert!(result.is_err());
-}
+    lib_main(vec!["a", "b", "-o", "d"])}
 
-#[test]
-fn get_output_filename_extracts_output_filename() {
-    let args = ["-o", "testOutputFilename"].to_vec();
-    let result = args.get_output_filename();
-    assert!(result == Some("testOutputFilename"));
-}
-
-#[test]
-fn get_output_filename_returns_none_when_given_insufficient_output_args() {
-    let args = vec!["-o"];
-    let result = args.get_output_filename();
-    assert!(result == None);
-}
-
-#[test]
-fn get_output_filename_returns_none_when_given_no_output_args() {
-    let args = vec!["-anything_but_o"];
-    let result = args.get_output_filename();
-    assert!(result == None);
-}
-
-#[test]
-fn remove_output_filename_switch_and_param() {
-    let args = vec!["-a", "-o", "remove_me", "-z"];
-    let result = args.remove_output_filename_switches_and_params();
-    assert!(!result.iter().any(|&el| el == "-o" || el == "remove_me"));
-    assert!(result.len() == 2);
-}
-
-#[test]
-fn remove_output_filename_switch_and_dash_o_param() {
-    let args = vec!["-a", "-o", "-o", "-z"];
-    let result = args.remove_output_filename_switches_and_params();
-    assert!(!result.iter().any(|&el| el == "-o" || el == "remove_me"));
-    assert!(result.len() == 2);
-}
-
-#[test]
-fn remove_output_filename_switch_and_two_dash_o_params() {
-    let args = vec!["-a", "-o", "remove_me", "-o", "-z"];
-    let result = args.remove_output_filename_switches_and_params();
-    assert!(!result.iter().any(|&el| el == "-o" || el == "remove_me"));
-    assert!(result.len() == 1);
-}
-
-#[test]
-fn remove_output_filename_switch_no_param() {
-    let args = vec!["-a", "-o"];
-    let result = args.remove_output_filename_switches_and_params();
-    assert!(!result.iter().any(|&el| el == "-o" || el == "remove_me"));
-    assert!(result.len() == 1);
-}
-
-#[test]
-fn remove_nostartfiles_no_defaultlibs_switches() {
-    let args = vec!["-a", "-nostartfiles", "-nostartfiles", "-y", "-z"];
-    let result = args.remove_nostartfiles_nodefaultlibs_switches();
-    assert!(!result.iter().any(|&el| el == "-nostartfiles"));
-    assert!(result.len() == 3);
-}
-
-#[test]
-fn remove_wl_switches() {
-    let args = vec!["-Wl,-a", "-b", "-wl,-c", "-Wl,-d", "-Wl,", "-Wl", "-g"];
-    let result = args.remove_wl_switches();
-    assert!(result.iter().any(|&el| el.starts_with("-Wl,") == false));
-    assert!(result.len() == 6);
-}
+//#[test]
+//fn get_output_filename_extracts_output_filename() {
+//    let args = ["-o", "testOutputFilename"].to_vec();
+//    let result = args.get_output_filename();
+//    assert!(result == Some("testOutputFilename"));
+//}
+//
+//#[test]
+//fn get_output_filename_returns_none_when_given_insufficient_output_args() {
+//    let args = vec!["-o"];
+//    let result = args.get_output_filename();
+//    assert!(result == None);
+//}
+//
+//#[test]
+//fn get_output_filename_returns_none_when_given_no_output_args() {
+//    let args = vec!["-anything_but_o"];
+//    let result = args.get_output_filename();
+//    assert!(result == None);
+//}
+//
+//#[test]
+//fn remove_output_filename_switch_and_param() {
+//    let args = vec!["-a", "-o", "remove_me", "-z"];
+//    let result = args.remove_output_filename_switches_and_params();
+//    assert!(!result.iter().any(|&el| el == "-o" || el == "remove_me"));
+//    assert!(result.len() == 2);
+//}
+//
+//#[test]
+//fn remove_output_filename_switch_and_dash_o_param() {
+//    let args = vec!["-a", "-o", "-o", "-z"];
+//    let result = args.remove_output_filename_switches_and_params();
+//    assert!(!result.iter().any(|&el| el == "-o" || el == "remove_me"));
+//    assert!(result.len() == 2);
+//}
+//
+//#[test]
+//fn remove_output_filename_switch_and_two_dash_o_params() {
+//    let args = vec!["-a", "-o", "remove_me", "-o", "-z"];
+//    let result = args.remove_output_filename_switches_and_params();
+//    assert!(!result.iter().any(|&el| el == "-o" || el == "remove_me"));
+//    assert!(result.len() == 1);
+//}
+//
+//#[test]
+//fn remove_output_filename_switch_no_param() {
+//    let args = vec!["-a", "-o"];
+//    let result = args.remove_output_filename_switches_and_params();
+//    assert!(!result.iter().any(|&el| el == "-o" || el == "remove_me"));
+//    assert!(result.len() == 1);
+//}
+//
+//#[test]
+//fn remove_nostartfiles_no_defaultlibs_switches() {
+//    let args = vec!["-a", "-nostartfiles", "-nostartfiles", "-y", "-z"];
+//    let result = args.remove_nostartfiles_nodefaultlibs_switches();
+//    assert!(!result.iter().any(|&el| el == "-nostartfiles"));
+//    assert!(result.len() == 3);
+//}
+//
+//#[test]
+//fn remove_wl_switches() {
+//    let args = vec!["-Wl,-a", "-b", "-wl,-c", "-Wl,-d", "-Wl,", "-Wl", "-g"];
+//    let result = args.remove_wl_switches();
+//    assert!(result.iter().any(|&el| el.starts_with("-Wl,") == false));
+//    assert!(result.len() == 6);
+//}
